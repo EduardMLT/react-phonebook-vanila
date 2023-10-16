2aaf1e5fd2a49a554f07ccf292359e5521369e58
EduardMLT <eddku@i.ua>
2023-10-16 21:38:49 +0300

    initial

--- src/index.js
+++ src/index.js
@@ -7,10 +7,8 @@ import { App } from 'components/App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>
-      
-        <App />
-      
+    <Provider store={store}>      
+        <App />      
     </Provider>
   </React.StrictMode>
 );
